1eb87ef219a20b968c2507ee11fd75b1d04cfed7
Tyler Moore <moore.2528@osu.edu>
2015-04-12 14:23:37 -0400

    Initial Commit

---
+++ CodeForACause/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  CodeForACause
+//
+//  Created by Tyler Moore on 4/12/15.
+//  Copyright (c) 2015 Tyler Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ CodeForACause/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CodeForACause
+//
+//  Created by Tyler Moore on 4/12/15.
+//  Copyright (c) 2015 Tyler Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
